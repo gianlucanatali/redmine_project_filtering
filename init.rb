0d284a07d877adc9f3330a9b4333cf36a15bb43a
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2014-07-08 10:47:13 +0200

    Bump version to 0.0.2

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :redmine_project_filtering do
   name 'Redmine Project Filtering plugin'
   author 'Antonio García-Domínguez'
   description 'Adds filtering to the project list'
-  version '0.0.1'
+  version '0.0.2'
   url 'http://github.com/bluezio/redmine_project_fitlering'
   author_url 'http://neptuno.uca.es/~agarcia'
 
